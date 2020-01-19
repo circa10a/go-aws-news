e9064b8d00f80cd8e5b06a61c42aea458c84c0fb
Caleb Lemoine <caleblemoine@gmail.com>
2020-01-19 16:52:11 -0600

    docs: add godoc examples

---
+++ news/examples_test.go
@@ -0,0 +1,65 @@
+package news
+
+import "fmt"
+
+func ExampleFetch() {
+	news, err := Fetch(2019, 12)
+	if err != nil {
+		// Handle error
+	}
+	news.Print()
+}
+
+func ExampleThisMonth() {
+	news, err := ThisMonth()
+	if err != nil {
+		// Handle error
+	}
+	for _, n := range news {
+		fmt.Println(n.Title)
+	}
+}
+
+func ExampleToday() {
+	news, err := Today()
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(news)
+}
+
+func ExampleYesterday() {
+	news, err := Yesterday()
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(news)
+}
+
+func (a Announcements) ExampleJSON() {
+	news, err := ThisMonth()
+	if err != nil {
+		// Handle error
+	}
+	json, jsonErr := news.Filter([]string{"S3"}).JSON()
+	if jsonErr != nil {
+		// Handle error
+	}
+	fmt.Println(string(json))
+}
+
+func (a Announcements) ExampleHTML() {
+	news, err := ThisMonth()
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(news.Filter([]string{"S3"}).HTML())
+}
+
+func (a Announcements) ExampleFilter() {
+	news, err := ThisMonth()
+	if err != nil {
+		// Handle error
+	}
+	fmt.Println(news.Filter([]string{"S3"}))
+}
